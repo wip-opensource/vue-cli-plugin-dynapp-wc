9725bb258c959cf46c1f06319e5c665d8a1185c3
Daniel Mattsson <danielm@wip.se>
2020-08-21 14:53:47 +0200

    Updated vue config with transpile dependencies setting

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './dist/' : '/'
-}
+  publicPath: process.env.NODE_ENV === 'production' ? './dist/' : '/',
+  transpileDependencies: [
+      "vuetify",
+      /node_modules\/dync-[a-zA-Z0-9]+/,
+      /node_modules\/@dynapp\//
+  ]
+};
