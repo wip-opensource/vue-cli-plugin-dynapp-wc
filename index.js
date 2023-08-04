b1990d064669d5c254a0d2e26495eff0f1e4c2c9
Tarek Auf der Strasse <tareks@wip.se>
2023-08-04 10:33:28 +0200

    Changed index.js to work with webpack 4.0+

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 module.exports = (api, options, rootOptions) => {
-  api.chainWebpack(config => {
-    config.devServer
-      .port(8181)
-      .host('0.0.0.0')
-      .hotOnly(true)
-      .disableHostCheck(true)
-      .clientLogLevel('warning')
-      .inline(true);
+  api.configureWebpack(config => {
+    config.devServer = {
+      port: 8181,
+      host: '0.0.0.0',
+      hot: true, // Equivalent to hotOnly(true) in Webpack 3
+      client: { logging: 'warn' },
+    };
   });
 };
