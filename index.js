41ac7c0dd55a18fa104503c51800379daf4034be
Tarek Auf der Strasse <tareks@wip.se>
2023-08-04 11:27:52 +0200

    removed error overlay

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = (api, options, rootOptions) => {
       port: 8181,
       host: '0.0.0.0',
       hot: true, // Equivalent to hotOnly(true) in Webpack 3
-      client: { logging: 'warn' },
+      client: { logging: 'warn',  overlay: false },
     };
   });
 };
