fbf0defa26dbbf425b0abd180cb05694c4b85932
Tarek Auf der Strasse <tareks@wip.se>
2023-08-07 10:42:00 +0200

    Fixed bug related to overwriting devServer settings

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 module.exports = (api, options, rootOptions) => {
   api.configureWebpack(config => {
     config.devServer = {
+      ...(config.devServer || {}),
       port: 8181,
       host: '0.0.0.0',
       hot: true, // Equivalent to hotOnly(true) in Webpack 3
       allowedHosts: "all",
       client: { logging: 'warn',  overlay: false },
+      
     };
   });
 };
